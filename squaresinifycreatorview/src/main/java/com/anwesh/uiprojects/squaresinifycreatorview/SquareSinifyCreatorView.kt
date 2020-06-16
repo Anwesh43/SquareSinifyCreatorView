ff3d02a8a688eaccfb9de4c1a56dfb1d05af0763
Anwesh43 <anweshthecool0@gmail.com>
2020-06-16 19:34:52 +0530

    addeing Canvas extension methods

--- squaresinifycreatorview/src/main/java/com/anwesh/uiprojects/squaresinifycreatorview/SquareSinifyCreatorView.kt
+++ squaresinifycreatorview/src/main/java/com/anwesh/uiprojects/squaresinifycreatorview/SquareSinifyCreatorView.kt
@@ -25,3 +25,34 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawSquareSinify(i : Int, scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sf : Float = scale.sinify()
+    val sc1 : Float = sf.divideScale(0, divs)
+    val sc2 : Float = sf.divideScale(1, divs)
+    val sc3 : Float = sf.divideScale(2, divs)
+    val sj : Float = 1f - 2 * i
+    save()
+    translate(w / 2, (h / 2) * sc1 - sj * size * 0.5f * sc3)
+    for (j in 0..1) {
+        save()
+        rotate(180f * j * sc2)
+        drawLine(0f, 0f, size * 0.5f, 0f, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawSSCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    save()
+    for (j in 0..1) {
+        drawSquareSinify(j, scale, w, h, paint)
+    }
+    restore()
+}
