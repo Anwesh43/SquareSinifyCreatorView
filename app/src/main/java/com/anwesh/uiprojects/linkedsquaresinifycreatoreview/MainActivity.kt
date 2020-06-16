7de289e4033484ed0720cf922f7a32a7bd65e6a9
Anwesh43 <anweshthecool0@gmail.com>
2020-06-16 19:51:03 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedsquaresinifycreatoreview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedsquaresinifycreatoreview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedsquaresinifycreatoreview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.squaresinifycreatorview.SquareSinifyCreatorView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SquareSinifyCreatorView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
