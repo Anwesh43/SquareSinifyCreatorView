64f7d36ad2c90e3dd35bdd9abfbf102bc72e8f60
Anwesh43 <anweshthecool0@gmail.com>
2020-06-16 15:29:24 +0530

    adding Float and Int extension methods

--- squaresinifycreatorview/src/main/java/com/anwesh/uiprojects/squaresinifycreatorview/SquareSinifyCreatorView.kt
+++ squaresinifycreatorview/src/main/java/com/anwesh/uiprojects/squaresinifycreatorview/SquareSinifyCreatorView.kt
@@ -20,3 +20,8 @@ val sizeFactor : Float = 3.8f
 val strokeFactor : Float = 90f
 val backColor : Int = Color.parseColor("#BDBDBD")
 val delay : Long = 20
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
