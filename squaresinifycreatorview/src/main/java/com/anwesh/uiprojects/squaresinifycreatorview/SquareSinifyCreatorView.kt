c23a9b13db12799c9ab2a210e2f950bc1fb3900c
Anwesh43 <anweshthecool0@gmail.com>
2020-06-16 19:36:03 +0530

    adding SquareSinifyCreatorView class

--- squaresinifycreatorview/src/main/java/com/anwesh/uiprojects/squaresinifycreatorview/SquareSinifyCreatorView.kt
+++ squaresinifycreatorview/src/main/java/com/anwesh/uiprojects/squaresinifycreatorview/SquareSinifyCreatorView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawSSCNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class SquareSinifyCreatorView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
